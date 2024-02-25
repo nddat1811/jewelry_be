75dc14ddcc93467fe5d2ba67aded055c4a313a4b
nddat1811 <63857350+nddat1811@users.noreply.github.com>
2024-02-25 08:13:10 +0700

    Revert "done address function"
    This reverts commit 1ae890f67f378c1637c643277b886c26b683658d.

--- src/helper/elasticsearch.ts
+++
@@ -1,6 +0,0 @@
-import { Client } from "@elastic/elasticsearch";
-
-
-export const elasticSearchClient = new Client({
-  node: 'http://localhost:9200', // Use "http://" protocol
-});
--- src/helper/hashPassword.ts
+++
@@ -1,8 +0,0 @@
-import * as bcrypt from "bcrypt";
-
-export function hashPassword(password: string): string {
-  const saltRounds = 10;
-  const salt = bcrypt.genSaltSync(saltRounds);
-  const hashedPassword = bcrypt.hashSync(password, salt);
-  return hashedPassword;
-}
